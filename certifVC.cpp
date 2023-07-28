d3225fef8b74c6d1f3f9d09de174c62e3efed19b
Fabrice Lécuyer <lecfab+git@gmail.com>
2023-07-28 03:20:52 +0200

    Update certifVC.cpp

--- certifVC.cpp
+++ certifVC.cpp
@@ -1,5 +1,5 @@
 // certifVC: Quality certification for Vertex Cover
-// Copyright (C) 2023 ...
+// Copyright (C) 2023 Fabrice Lécuyer (fabrice.lecuyer@lip6.fr)
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
